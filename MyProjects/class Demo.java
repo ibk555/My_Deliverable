252ee3666d5c36883e23cd43bebb8242f7330eaf
Araromi Enoch <67438525+ibk555@users.noreply.github.com>
2025-01-30 10:13:31 +0100

    Add files via upload

---
+++ MyProjects/class Demo.java
@@ -0,0 +1,9 @@
+class Demo {
+    Public static void main(String)[]{
+        int age = 30;
+        if (age < 16){
+            System.out.printLn("Too Young"):
+            System.out.println("Welcome!"):
+        }
+    }
+}
