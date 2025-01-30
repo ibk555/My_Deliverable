fbdebef8bcd7a2c7441f1f8ecc901b498846bea7
Araromi Enoch <67438525+ibk555@users.noreply.github.com>
2025-01-30 10:14:29 +0100

    Delete MyProjects/class Demo.java

--- MyProjects/class Demo.java
+++
@@ -1,9 +0,0 @@
-class Demo {
-    Public static void main(String)[]{
-        int age = 30;
-        if (age < 16){
-            System.out.printLn("Too Young"):
-            System.out.println("Welcome!"):
-        }
-    }
-}
